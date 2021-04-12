b833384602f14d56c42825772d19cd2e2de34a93
denimendica678 <58472463+denimendica678@users.noreply.github.com>
2021-04-12 09:21:50 +0200

    Add files via upload

---
+++ niopTest/niopTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+namespace SpremanjeTeksta
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ciljnaDatoteka = Path.Combine(@"D:\My Documents\testNiop\MendicaDeniSystemNiOP.csv");
+
+            string ime, prezime, email;
+            int gidRod;
+
+            Console.WriteLine("Unesite svoje ime");
+            Console.WriteLine("Unesite svoje prezime");
+            Console.WriteLine("Unesite svoju godinu rođenja");
+            Console.WriteLine("Unesite svoj e-mail");
+
+            try
+            {
+                using (System.IO.StreamWriter file = new System.IO.StreamWriter(@filepath, true))
+                {
+                    file.WriteLine(ime + "" + prezime + "" + godRod + "" + email);
+                }
+            }
+            catch(Exception ex)
+            {
+
+            }
+        }
+    }
+}
